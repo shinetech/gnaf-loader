f68e7c9352aea83d642376ce061a81f79677ba35
duc-duong-energetiq <duc.duong@shineproducts.com.au>
2020-07-08 19:33:27 +1000

    SDR-1140: generate postgres dump files from gnaf

---
+++ download.sh
@@ -0,0 +1,7 @@
+curl --url "$GNAF_DOWNLOAD_URL" --output ./data/GNAP.zip
+curl --url "$ADMIN_BDYS_DOWNLOAD_URL" --ouput ./data/ADMIN_BDYS.zip
+
+for d in ./data/*.zip
+do
+  unzip -d ./data "$d"
+done
