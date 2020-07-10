aee04d4ac5618301c7831c2322d0941d06933489
duc-duong-energetiq <duc.duong@shineproducts.com.au>
2020-07-10 12:07:32 +1000

    SDR-1140: python 3.6 docker image

--- ops/import.sh
+++ ops/import.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-printenv
-
 psql --version
 
-while ! psql -l; do
+while ! psql -l > /dev/null; do
   echo "** Waiting for connection with RDS to start up and be ready for queries. **"
   sleep 10
 done
 
+echo "** Database connection is ready to use **"
+
 if [ ! "$GNAF_DUMP_URL" = "" ]; then
+  echo "** Now downloading GNAF dump **"
   curl --url "$GNAF_DUMP_URL" --output ./data/GNAP.dump
   echo "** Now importing GNAF dump into RDS **"
   # pg_restore -Fc /data/GNAP.dmp
 fi
 
 if [ ! "$ADMIN_BDYS_DUMP_URL" = "" ]; then
+  echo "** Now downloading ADMIN_BDYS dump **"
   curl --url "$ADMIN_BDYS_DUMP_URL" --output ./data/ADMIN_BDYS.dump
   echo "** Now importing ADMIN_BDYS dump into RDS **"
   # pg_restore -Fc /data/ADMIN_BDYS.dmp
