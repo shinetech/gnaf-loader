553aae5a882b0359b52fccb109789bb00c33d1ed
duc-duong-energetiq <duc.duong@shineproducts.com.au>
2020-07-10 20:10:27 +1000

    SDR-1140: checking required extensions before restoring

--- ops/import.sh
+++ ops/import.sh
@@ -10,6 +10,21 @@ done
 echo "** Database connection is ready to use **"
 psql -l
 
+REQUIRE_EXTENSIONS_COUNT=4
+EXTENSIONS=$(psql -d "$PGDATABASE" -p "$PGPORT" -U "$PGUSER" -c "\dx")
+echo "$EXTENSIONS";
+CHECK_EXTENSION=$(echo "$EXTENSIONS" | grep -E 'postgis|fuzzystrmatch|postgis_tiger_geocoder|postgis_topology' -c)
+
+echo "Found $CHECK_EXTENSION/$REQUIRE_EXTENSIONS_COUNT required extension(s) installed!"
+
+if [ "$CHECK_EXTENSION" -lt $REQUIRE_EXTENSIONS_COUNT ]; then
+  echo "ERROR: Extensions count should be at least $REQUIRE_EXTENSIONS_COUNT, but found only $CHECK_EXTENSION"
+  echo "ERROR: Required extensions were not installed properly or missing! Now exitting..."
+  exit 1;
+fi
+
+echo "All required extensions are found, now processing data files..."
+
 if [ ! "$GNAF_DUMP_URL" = "" ]; then
   echo "** Now downloading GNAF dump **"
   curl --url "$GNAF_DUMP_URL" --output ./data/GNAP.dmp
